d150f8400f7f364a5ea9483f9b81d641f411f978
Radu Mantu <andru.mantu@gmail.com>
2025-10-30 16:29:32 +0200

    Modified create_vlan_tag() to include VID extension
    PCP and DEI bits are now used to extend VID.
    The new vlan ID will coincide with the TCI that the Ethernet parser will
    now return.

--- switch.py
+++ switch.py
@@ -11,23 +11,32 @@ def parse_ethernet_header(data):
     #dest_mac, src_mac, ethertype = struct.unpack('!6s6sH', data[:14])
     dest_mac = data[0:6]
     src_mac = data[6:12]
-    
+
     # Extract ethertype. Under 802.1Q, this may be the bytes from the VLAN TAG
     ether_type = (data[12] << 8) + data[13]
 
     vlan_id = -1
+    vlan_tci = -1
     # Check for VLAN tag (0x8100 in network byte order is b'\x81\x00')
     if ether_type == 0x8200:
         vlan_tci = int.from_bytes(data[14:16], byteorder='big')
         vlan_id = vlan_tci & 0x0FFF  # extract the 12-bit VLAN ID
         ether_type = (data[16] << 8) + data[17]
 
-    return dest_mac, src_mac, ether_type, vlan_id
+    return dest_mac, src_mac, ether_type, vlan_id, vlan_tci
 
-def create_vlan_tag(vlan_id):
-    # 0x8100 for the Ethertype for 802.1Q
-    # vlan_id & 0x0FFF ensures that only the last 12 bits are used
-    return struct.pack('!H', 0x8200) + struct.pack('!H', vlan_id & 0x0FFF)
+def create_vlan_tag(ext_id, vlan_id):
+    # Use EtherType = 8200h for our custom 802.1Q-like protocol.
+    # PCP and DEI bits are used to extend the original VID.
+    #
+    # The ext_id should be the sum of all nibbles in the MAC address of the
+    # host attached to the _access_ port. Ignore the overflow in the 4-bit
+    # accumulator.
+    #
+    # NOTE: Include these 4 extensions bits only in the check for unicast
+    #       frames. For multicasts, assume that you're dealing with 802.1Q.
+    return struct.pack('!H', 0x8200) + \
+           struct.pack('!H', ((ext_id & 0xF) << 12) | (vlan_id & 0x0FFF))
 
 def function_on_different_thread():
     while True:
@@ -60,14 +69,14 @@ def main():
         # b3 = b1[0:2] + b[3:4].
         interface, data, length = recv_from_any_link()
 
-        dest_mac, src_mac, ethertype, vlan_id = parse_ethernet_header(data)
+        dest_mac, src_mac, ethertype, vlan_id, vlan_tci = parse_ethernet_header(data)
 
         # Print the MAC src and MAC dst in human readable format
         dest_mac = ':'.join(f'{b:02x}' for b in dest_mac)
         src_mac = ':'.join(f'{b:02x}' for b in src_mac)
 
         # Note. Adding a VLAN tag can be as easy as
-        # tagged_frame = data[0:12] + create_vlan_tag(10) + data[12:]
+        # tagged_frame = data[0:12] + create_vlan_tag(5, 10) + data[12:]
 
         print(f'Destination MAC: {dest_mac}')
         print(f'Source MAC: {src_mac}')
